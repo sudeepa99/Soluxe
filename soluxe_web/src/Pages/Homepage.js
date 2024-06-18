b5dc70c83cb3385780b0e90674a3337a68d18ac4
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-18 10:09:25 +0530

    Added navigation bar

--- soluxe_web/src/Pages/Homepage.js
+++ soluxe_web/src/Pages/Homepage.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Navigationbar from '../Components/Navigationbar';
+
 
 export default function Homepage() {
   return (
     <div>
-        <h1>Homepage</h1>
+      <Navigationbar/>
+        
       
     </div>
   );
--- soluxe_web/src/Pages/New.js
+++ soluxe_web/src/Pages/New.js
@@ -3,6 +3,9 @@ import React from 'react'
 export default function New() {
   return (
     <div>
+      <h1>
+        New
+      </h1>
       
     </div>
   )
