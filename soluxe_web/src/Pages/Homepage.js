eef1c836272251e46b82eb878432bda1616b308f
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-20 23:37:36 +0530

    Added styles to card collection

--- soluxe_web/src/Pages/Homepage.js
+++ soluxe_web/src/Pages/Homepage.js
@@ -13,6 +13,7 @@ import image_women_shoe2 from '../Assets/woman-shoes.jpg';
 import image_women_shoe3 from '../Assets/shoes-footwear-woman.jpg';
 import image_women_shoe4 from '../Assets/woman-shoes new.jpg';
 import image_women_shoe5 from '../Assets/high-heels-black-velvet.jpg';
+import './Homepage.css';
 
 
 
@@ -24,19 +25,19 @@ import image_women_shoe5 from '../Assets/high-heels-black-velvet.jpg';
 
 export default function Homepage() {
   return (
-    <div>
+    <div >
       <Navigationbar/>
       <ImageSlider/>
       <Content/>
-      <div>
-      //men shoes
+      <div className='card_collection'>
+      {/* men shoes */}
       <CardDesign image_name={image_men_shoe1} shoe_value="200 $" detail="jsjksksksk"/>
       <CardDesign image_name={image_men_shoe2} shoe_value="350 $" detail="jsjksksksk"/>
       <CardDesign image_name={image_men_shoe3} shoe_value="400 $" detail="jsjksksksk"/>
       <CardDesign image_name={image_men_shoe4} shoe_value="300 $" detail="jsjksksksk"/>
       <CardDesign image_name={image_men_shoe5} shoe_value="500 $" detail="jsjksksksk"/>
 
-      //women shoes
+      {/* women shoes */}
 
       <CardDesign image_name={image_women_shoe1} shoe_value="500 $" detail="jsjksksksk"/>
       <CardDesign image_name={image_women_shoe2} shoe_value="500 $" detail="jsjksksksk"/>
