fcd56562ee3ef8e213d31fdf9ecf63ea32664dae
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-21 23:56:26 +0530

    Added stylings to feature cards

--- soluxe_web/src/Components/Featured.js
+++ soluxe_web/src/Components/Featured.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import Card from 'react-bootstrap/Card';
+import './Featured.css';
 
 export default function Featured(props) {
   return (
     <div>
-        <Button onClick="" >
+        <Button className='featured_button' onClick="" >
         <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={props.Image_path} />
       <Card.Body>
