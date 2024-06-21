ca00a61da40e690102782fea59d4528227f2c120
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-22 00:31:09 +0530

    added some sort of stylings to footer

--- soluxe_web/src/Components/Footer.js
+++ soluxe_web/src/Components/Footer.js
@@ -1,20 +1,21 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import './Footer.css';
 
 export default function Footer() {
   return (
     <div className='container_footer'>
-        <div className='row1'>
-            <Button>About us</Button>
-            <Button>Customer Service</Button>
+        <div className='row'>
+            <Button className='footer_button'>About us</Button>
+            <Button className='footer_button'>Customer Service</Button>
         </div>
-        <div className='row2'>
-            <Button>Our Story</Button>
-            <Button>Need Help?</Button>
+        <div className='row'>
+            <Button className='footer_button'>Our Story</Button>
+            <Button className='footer_button'>Need Help?</Button>
         </div>
-        <div className='row3'>
-            <Button>Reviews</Button>
-            <Button>Size Guide</Button>
+        <div className='row'>
+            <Button className='footer_button'>Reviews</Button>
+            <Button className='footer_button'>Size Guide</Button>
         </div>
         
 
