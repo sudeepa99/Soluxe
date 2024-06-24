52555cc85fbb4c177444ee9d3858698bc4fb531c
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-24 11:45:50 +0530

    Added content to new page

--- soluxe_web/src/Pages/New.js
+++ soluxe_web/src/Pages/New.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navigationbar from '../Components/Navigationbar'
+import Content_new from '../Components/Content_new'
 
 export default function New() {
   return (
     <div>
       <div>
         <Navigationbar/>
+        <Content_new/>
       </div>
       
     </div>
