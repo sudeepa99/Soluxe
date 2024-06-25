7d20c776d5d263b703beaf1f69c6f4bb0a4a9752
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-25 23:57:25 +0530

    Added navigation bar to the women page

--- soluxe_web/src/Pages/Women.js
+++ soluxe_web/src/Pages/Women.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigationbar from '../Components/Navigationbar'
 
 export default function Women() {
   return (
     <div>
+      <Navigationbar/>
       
     </div>
   )
