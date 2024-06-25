ca211f1a1df534b459a568265ed50fec1479893a
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-06-25 23:40:31 +0530

    Added navigation bar to the men page

--- soluxe_web/src/Pages/Men.js
+++ soluxe_web/src/Pages/Men.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigationbar from '../Components/Navigationbar'
 
 export default function Men() {
   return (
     <div>
+      <Navigationbar/>
       
     </div>
   )
