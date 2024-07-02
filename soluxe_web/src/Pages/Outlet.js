b3a1de67e0739bf26b5583fed72f6a3cbff1ea63
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-02 22:54:57 +0530

    added card design outlet to outlet page

--- soluxe_web/src/Pages/Outlet.js
+++ soluxe_web/src/Pages/Outlet.js
@@ -1,10 +1,33 @@
 import React from 'react'
-import Content_outlet from '../Components/Content_outlet'
+import Content_outlet from '../Components/Content_outlet';
+import image_men_shoe1 from '../Assets/brown-leather-shoes.jpg';
+import image_men_shoe2 from '../Assets/shoes.jpg';
+import image_men_shoe3 from '../Assets/classic-stylish-men-s-shoes.jpg';
+import image_men_shoe4 from '../Assets/men-shoes.jpg';
+import image_men_shoe5 from '../Assets/classic-stylish-men-s-shoes-closeup-gray.jpg';
+import image_women_shoe1 from '../Assets/fashion-pink-glamour-women-heels.jpg';
+import image_women_shoe2 from '../Assets/woman-shoes.jpg';
+import image_women_shoe3 from '../Assets/shoes-footwear-woman.jpg';
+import image_women_shoe4 from '../Assets/woman-shoes new.jpg';
+import image_women_shoe5 from '../Assets/high-heels-black-velvet.jpg';
+import CardDesign_outlet from '../Components/CardDesign_outlet';
 
 export default function Outlet() {
   return (
     <div>
       <Content_outlet/>
+
+      <CardDesign_outlet image_name={image_men_shoe1} shoe_value="200$" detail="anjksksks" tag_name="Men"/>
+      <CardDesign_outlet image_name={image_men_shoe2} shoe_value="200$" detail="anjksksks" tag_name="Men"/>
+      <CardDesign_outlet image_name={image_men_shoe3} shoe_value="200$" detail="anjksksks" tag_name="Men"/>
+      <CardDesign_outlet image_name={image_men_shoe4} shoe_value="200$" detail="anjksksks" tag_name="Men"/>
+      <CardDesign_outlet image_name={image_men_shoe5} shoe_value="200$" detail="anjksksks" tag_name="Men"/>
+
+      <CardDesign_outlet image_name={image_women_shoe1} shoe_value="200$" detail="anjksksks" tag_name="Women"/>
+      <CardDesign_outlet image_name={image_women_shoe2} shoe_value="200$" detail="anjksksks" tag_name="Women"/>
+      <CardDesign_outlet image_name={image_women_shoe3} shoe_value="200$" detail="anjksksks" tag_name="Women"/>
+      <CardDesign_outlet image_name={image_women_shoe4} shoe_value="200$" detail="anjksksks" tag_name="Women"/>
+      <CardDesign_outlet image_name={image_women_shoe5} shoe_value="200$" detail="anjksksks" tag_name="Women"/>
       
     </div>
   )
