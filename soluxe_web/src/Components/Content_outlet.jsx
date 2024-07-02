9852adccbacea1b8c23b4ec8cb5181a2e468806a
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-02 22:03:58 +0530

    added content outlet to the outlet page

--- soluxe_web/src/Components/Content_outlet.jsx
+++ soluxe_web/src/Components/Content_outlet.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './Content_new.css';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 export default function Content_outlet() {
   return (
-    <div>
-      
+    <div className='body_contentnew'>
+      <div className='head_new'>
+        <h1>Sale</h1>
+      </div>
+      <div className='contennew_drop'>
+      <Dropdown >
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Sort By
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Best Match</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Price: Low to High</Dropdown.Item>
+        <Dropdown.Item href="#/action-3">Price: High to Low</Dropdown.Item>
+        <Dropdown.Item href="#/action-4">Newest Arrivals</Dropdown.Item>
+
+      </Dropdown.Menu>
+    </Dropdown>
+      </div>
     </div>
   )
 }
--- soluxe_web/src/Pages/Outlet.js
+++ soluxe_web/src/Pages/Outlet.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Content_outlet from '../Components/Content_outlet'
 
 export default function Outlet() {
   return (
     <div>
+      <Content_outlet/>
       
     </div>
   )
