e9fb993b57c37974972178d8c3e8021f7d88562f
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-03 13:52:46 +0530

    started to work in our story page

--- soluxe_web/src/Pages/OurStory.js
+++ soluxe_web/src/Pages/OurStory.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navigationbar from '../Components/Navigationbar'
 
 export default function OurStory() {
   return (
     <div>
+      <Navigationbar/>
       
     </div>
   )
