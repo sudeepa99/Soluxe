7240133e06fb9d2d32a8a2bbfc67654342ab92fb
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-03 14:28:29 +0530

    Added banner image to the story page

--- soluxe_web/src/Pages/OurStory.js
+++ soluxe_web/src/Pages/OurStory.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navigationbar from '../Components/Navigationbar'
+import BannerImage from "../Assets/Banner_Image.png";
+import { Image } from 'react-bootstrap';
+import "./OurStory.css";
 
 export default function OurStory() {
   return (
     <div>
       <Navigationbar/>
+      <Image className="banner_img" src={BannerImage}></Image>
       
     </div>
   )
