a4407c7a2003bfd04d619ebcf64c3a3ab168a343
sudeepa99 <sudeepa1234nisal@gmail.com>
2024-07-04 23:58:34 +0530

    Done some  work in story container4

--- soluxe_web/src/Components/Story_container4.jsx
+++ soluxe_web/src/Components/Story_container4.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import { Image } from 'react-bootstrap';
 import Image1 from "../Assets/image0.jpg";
+import Image2 from "../Assets/image1.jpg";
+import "./Story_container4.css";
 
 
 export default function Story_container4() {
   return (
-    <div>
-        <div>
-            <Image src={Image1}/>
+    <div className='story_container4'>
+            <div>
+                <Image className='image_customer' src={Image1}/>
+                <div style={{color:"black", fontWeight:"bold", textAlign:"center"}}>
+                    Happy Customer
+                </div>
 
-        </div>
+            </div>
+            <div>
+                <Image className='image_customer' src={Image2}/>
+                <div style={{color:"black", fontWeight:"bold", textAlign:"center"}}>
+                    Happy Customer
+                </div>
+
+            </div>
+            
       
     </div>
   )
