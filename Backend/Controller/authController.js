187a0838fc8155dfef62e2fcd946a83d8eec34b2
sudeepa99 <95671202+sudeepa99@users.noreply.github.com>
2024-07-29 21:55:27 +0530

    Update authController.js

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -1,4 +1,5 @@
 const UserSchema = require("../Models/auth");
+const bcrypt = require('bcrypt');
 const UserRegister = async (req,res)=>{
     const {name,email,password}=req.body;
 
@@ -8,7 +9,11 @@ const UserRegister = async (req,res)=>{
         if(user){
             return res.status(400).json({Success:false,message:"User Already Exist"}); 
         }
-        user = new UserSchema({name,email,password});
+
+        const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+        user = new UserSchema( { name, email, password: hashPassword  });
         await user.save();
 
         return res.status(200).json({Success:true,message:"User Created"}); 
@@ -20,5 +25,36 @@ const UserRegister = async (req,res)=>{
         
     }
 }
+const UserLogin = async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      let user = await UserSchema.findOne({ email });
+  
+      if (!user) {
+        return res.status(404).json({ success: false, message: 'User not found' });
+      }
+  
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordMatch) {
+        return res.status(400).json({ success: false, message: 'Invalid credentials' });
+      }
 
-module.exports={UserRegister}
+      res.status(200).json({
+        success: true,
+        message: 'Successfully logged in',
+        data: user,
+      });
+  
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ success: false, message: 'Internal server error, please try again' });
+    }
+  };
+  
+  module.exports = {
+    UserRegister,
+    UserLogin,
+  };
+  
