d74bcd985692f717efedbf64b349d9f448514b33
Sudeepa Nisal <95671202+sudeepa99@users.noreply.github.com>
2024-08-07 18:06:14 +0530

    Update Login.jsx

--- soluxe_web/src/Pages/Login.jsx
+++ soluxe_web/src/Pages/Login.jsx
@@ -1,15 +1,54 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Button, Image } from 'react-bootstrap';
 import Logo from "../Assets/logo_soluxe.png";
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
 import "./Login.css";
 import Navigationbar from '../Components/Navigationbar';
 import { useNavigate } from 'react-router-dom';
-export default function Login() {
-    const navigate = useNavigate();
+const  Login=()=>{
+    const [formData, setFormData] = useState({
+        password: '',
+        email: '',
+      });
+      const [error, setError] = useState(null);
+      const navigate = useNavigate();
+    
+      const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: value,
+        }));
+      };
+    
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        loging(formData);
+      };
 
-    const handleUserSignupClick =() =>{
-        navigate('/signup');
-    }
+      const loging = (data) => {
+        Axios.post('http://localhost:3001/api/login', data)
+          .then((response) => {
+            const { token, email } = response.data;
+            console.log(`Login: ${token}`);
+    
+            if (token) {
+              localStorage.setItem('authToken', token);
+              setError(null);
+              navigate('/');
+            } else {
+              setError('Login failed: no token received');
+            }
+          })
+          .catch((error) => {
+            if (error.response && error.response.status === 400) {
+              setError(error.response.data.error);
+            } else {
+              setError('An unexpected error occurred');
+            }
+          });
+      };    
 
   return (
     <div>
@@ -18,7 +57,7 @@ export default function Login() {
             <Image className='logo' src={Logo}/>
             <h1 className='log_in_header'>LOGIN</h1>
 
-            <form>
+            <form className="Auth-form" onSubmit={handleSubmit}>
                 <div className="form_one">
                 <div className='input_fields'>
                     <label>Email</label>
@@ -26,21 +65,39 @@ export default function Login() {
                 </div>
 
                 <div className='input_fields'>
-                    <input type='email' name='email'/>
-                    <input type='email' name='email'/>
-                </div>
-
-                </div>
+            <input
+              type="email"
+              className="form-control mt-1"
+              placeholder="Enter email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              required
+            />                    
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              required
+            />                
+            </div>
+            </div>
                 
 
                 <Button className="login_button" type='submit' >Login</Button>
             </form>
             <div className='bottom_buttons'>
                 <Button className="login_button2">Forgot Password</Button>
-                <Button className="login_button2" onClick={handleUserSignupClick}>No Account? Sign Up</Button>
+                <Button className="login_button2"><Link to="/signup">No Account? Sign Up</Link></Button>
             </div>
         </div>
       
     </div>
   )
 }
+
+export default Login;
+
