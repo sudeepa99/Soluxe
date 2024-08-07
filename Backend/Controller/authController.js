8ac2794e86bf9284dac095d93cee10e6a855803b
Sudeepa Nisal <95671202+sudeepa99@users.noreply.github.com>
2024-08-07 18:05:37 +0530

    Update authController.js

--- Backend/Controller/authController.js
+++ Backend/Controller/authController.js
@@ -1,5 +1,12 @@
 const UserSchema = require("../Models/auth");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const generateToken = user => {
+  return jwt.sign({ id: user._id, email:user.email }, process.env.JWT_SECRET_KEY, {
+    expiresIn: '15d' 
+  });
+};
 const UserRegister = async (req,res)=>{
     const {name,email,password}=req.body;
 
@@ -26,32 +33,39 @@ const UserRegister = async (req,res)=>{
   
 }
 const UserLogin = async (req, res) => {
-    const { email, password } = req.body;
-  
-    try {
-      let user = await UserSchema.findOne({ email });
-  
-      if (!user) {
-        return res.status(404).json({ success: false, message: 'User not found' });
-      }
-  
-      const isPasswordMatch = await bcrypt.compare(password, user.password);
-  
-      if (!isPasswordMatch) {
-        return res.status(400).json({ success: false, message: 'Invalid credentials' });
-      }
+   const { email, password } = req.body;
 
-      res.status(200).json({
-        success: true,
-        message: 'Successfully logged in',
-        data: user,
-      });
-  
-    } catch (err) {
-      console.error(err.message);
-      res.status(500).json({ success: false, message: 'Internal server error, please try again' });
+  try {
+    let user = await UserSchema.findOne({ email }) || await Admin.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
     }
-  };
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(400).json({ success: false, message: 'Invalid credentials' });
+    }
+
+    const token = generateToken(user);
+
+    console.log(`Generate: ${token}`);
+    const { password: pwd, ...userData } = user._doc;
+
+    res.status(200).json({
+      success: true,
+      message: 'Successfully logged in',
+      token,
+      data: userData,
+    });
+
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ success: false, message: 'Internal server error, please try again' });
+  }
+};
+
   
   module.exports = {
     UserRegister,
