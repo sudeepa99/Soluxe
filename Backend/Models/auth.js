fa6388e85c0cb5f57c3a5595f4f25150b15006f8
Sudeepa Nisal <95671202+sudeepa99@users.noreply.github.com>
2024-08-07 17:37:56 +0530

    Update auth.js

--- Backend/Models/auth.js
+++ Backend/Models/auth.js
@@ -1,31 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require ("mongoose")
 const Schema = mongoose.Schema;
-
-const cartSchema = new Schema({
-  totalAmount: {
-    type: Number,
-    default: 0,
-  },
+const UserSchema = new Schema({
+    name:{type:String, required:true},
+    email:{type:String, required:true,unique:true},
+    password:{type:String, required:true,unique:true},
 });
 
-const userSchema = new Schema({
-  name:  { type: String, required: true },
-  email:  { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-  address:  { type: String, required: true },
-  country:  { type: String, required: true },
-  phoneNumber:  { type: String, required: true },
-  postalCode: { type: Number },
-  role: {
-    type: String,
-    default: 'user',
-  },
-  cart: {
-    type: cartSchema,
-    default: () => ({}),
-  },
-});
-
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports=mongoose.model("User",UserSchema)
